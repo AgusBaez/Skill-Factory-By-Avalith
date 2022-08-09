5e9bf8c0401e6be1c4ca54598a42260b0d3783c8
AgusBaez <agustinbaezignacio@gmail.com>
2022-08-09 14:35:40 -0300

    Upload storeModel

---
+++ Class/Clase10/Exercise/model/storeModel.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+//"""""Conexion a la base de datos"""""
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  return await res.json();
+}
+
+async function getCategories() {
+  const res = await fetch("https://fakestoreapi.com/products/categories");
+  return await res.json();
+}
+
+async function getByCategories(category) {
+  const res = await fetch(`https://fakestoreapi.com/products/category/` + category);
+  return await res.json();
+}
+
+async function getUsers() {
+  const res = await fetch("https://fakestoreapi.com/users/");
+  return await res.json();
+}
+
+async function get3Users() {
+  const res = await fetch("https://fakestoreapi.com/users/?limit=3");
+  return await res.json();
+}
+
+async function getCarts() {
+  const res = await fetch("https://fakestoreapi.com/carts");
+  return await res.json();
+}
+
+//DECLARACION DE OBJETO PARA EXPORTACION DE FUNCIONES
+let product = {
+  getProducts,
+  getCategories,
+  getByCategories,
+  getUsers,
+  get3Users,
+  getCarts,
+  //getBigCarts,
+};
+
+module.exports = product;
